b25d49ef0d23fae63ec5770de6a51cdf696285aa
alifhs <alif.hs@outlook.com>
2021-07-27 23:24:33 +0600

    added sectionlist

--- App.js
+++ App.js
@@ -1,51 +1,82 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, Button, FlatList} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  FlatList,
+  RefreshControl,
+  SectionList,
+} from 'react-native';
 
 const App = () => {
-  const [Items, setItems] = useState([
-    {name: 'Item 1'},
-    {name: 'Item 2'},
-    {name: 'Item 3'},
-    {name: 'Item 4'},
-    {name: 'Item 5'},
-    {name: 'Item 6'},
-    {name: 'Item 7'},
-    {name: 'Item 8'},
-    {name: 'Item 9'},
-    {name: 'Item 27'},
-    {name: 'Item 78'},
+  const [DATA, setDATA] = useState([
+    {
+      title: 'Title 1',
+      data: ['Item 1-1', 'Item 1-2'],
+    },
   ]);
+  const onRefresh = () => {
+    setRefreshing(true);
+    const index = DATA.length + 1;
+    setDATA([
+      ...DATA,
+      {
+        title: 'Title ' + index,
+        data: [`Item ${index}-1`, 'Item ' + index + '-2'],
+      },
+    ]);
+    setRefreshing(false);
+  };
+  const [Refreshing, setRefreshing] = useState(false);
+
   return (
-    <FlatList
+    <SectionList
       keyExtractor={(item, index) => index.toString()}
-      data={Items}
+      sections={DATA}
       renderItem={({item}) => (
         <View style={styles.item}>
-          <Text style={styles.text}>{item.name}</Text>
+          <Text style={styles.text_item}>{item}</Text>
         </View>
       )}
+      renderSectionHeader={({section}) => (
+        <View style={styles.header}>
+          <Text style={styles.text_header}>{section.title}</Text>
+        </View>
+      )}
+      refreshControl={
+        <RefreshControl
+          refreshing={Refreshing}
+          onRefresh={onRefresh}
+          colors={['#ff00ff']}
+        />
+      }
     />
   );
 };
 
 const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#ffffff',
-  },
-  item: {
-    margin: 10,
+  header: {
     backgroundColor: '#4ae1fa',
     justifyContent: 'center',
     alignItems: 'center',
+    borderWidth: 2,
   },
-  text: {
+  item: {
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text_header: {
     color: '#000000',
     fontSize: 45,
     fontStyle: 'italic',
     margin: 10,
   },
+  text_item: {
+    color: '#000000',
+    fontSize: 35,
+    margin: 5,
+  },
 });
-
 export default App;
